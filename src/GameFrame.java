b9181fe743209c2e0ed3beb216c4d42f176be572
Camellia Peng <camelliapeng02@gmail.com>
2016-01-01 17:12:08 +0800

    new game

---
+++ src/GameFrame.java
@@ -0,0 +1,13 @@
+import java.awt.Canvas;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends Canvas{
+
+    private static final long serialVersionUID = -8519965885188987594L;
+    
+    public GameFrame(int width, int height, String title){
+        JFrame frame = new JFrame(title);
+    }
+    
+}
