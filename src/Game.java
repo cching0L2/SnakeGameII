cee7ddcac17b18becd983f1a41e9ac977842b8b4
Camellia Peng <camelliapeng02@gmail.com>
2016-01-04 23:19:28 +0800

    fixed draw string delay bug

--- src/Game.java
+++ src/Game.java
@@ -2,10 +2,13 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferStrategy;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.ImageIcon;
 import javax.swing.Timer;
@@ -26,8 +29,9 @@ public class Game extends Canvas implements Runnable {
     public static final int GB_X = GRID_SIZE * 2, GB_Y = GRID_SIZE * 5;
     public static final int GB_WIDTH = 480, GB_HEIGHT = 480;
     private static final Color GBColor = new Color(204, 229, 255);
+    private static final Color NoticeColor = new Color(153, 204, 255);
     private static final Color GBBorderColor = new Color(106, 90, 205);
-    private final ImageIcon gameOverImage = Util.createImageIcon("game over image", "image/game_over.gif");
+    private static final Font UIFont = new Font("Eurostile", Font.PLAIN, 16);
 
     private KeyController keyController;
     private Handler handler;
@@ -86,12 +90,14 @@ public class Game extends Canvas implements Runnable {
     private void render() {
         BufferStrategy bs = this.getBufferStrategy();
         if (bs == null) {
-            this.createBufferStrategy(3);
+            this.createBufferStrategy(2);
             return;
         }
 
         Graphics g = bs.getDrawGraphics();
 
+        g.setFont(UIFont);
+        
         // background
         g.setColor(UIColor);
         g.fillRect(0, 0, WIDTH, HEIGHT);
@@ -117,8 +123,20 @@ public class Game extends Canvas implements Runnable {
             handler.render(g);
         }
         if(gameState == State.Over){
-            g.setFont(new Font("Courier", Font.BOLD, 30));
-            g.drawImage(gameOverImage.getImage(), 150, 250, this);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            g.setColor(NoticeColor);
+            g.fillRect(GB_X+(GB_WIDTH-200)/2, GB_Y+(GB_HEIGHT-100)/4, 200, 100);
+            g.setColor(GBBorderColor);
+            g.drawRect(GB_X+(GB_WIDTH-200)/2, GB_Y+(GB_HEIGHT-100)/4, 200, 100);
+            Graphics2D g2d = (Graphics2D)g;
+            g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                    RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+            g.drawString("Game over...", 175, 185);
+            g.drawString("You lost with a score of: ", 175, 205);
         }
 
         g.dispose();
