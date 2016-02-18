ba308a98ba113c46da013fdb73ded70218d308fa
Camellia Peng <camelliapeng02@gmail.com>
2016-02-17 23:13:39 -0800

    added new obstacle class: flower patch

---
+++ src/game_elements/FlowerPatch.java
@@ -0,0 +1,46 @@
+package game_elements;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+
+import game_UI.Game;
+import game_control.Position;
+import game_control.Util;
+
+public class FlowerPatch extends ObstacleElements{
+
+    Position position;
+    int HEIGHT = 1, LENGTH = 1;
+    static ImageIcon bushImage = Util.createImageIcon("flower", "../image/flower.gif");
+    
+    public FlowerPatch(Position position){
+        this.position = position;
+    }
+    
+    @Override
+    public void tick() {
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(bushImage.getImage(), position.getX()*Game.GRID_SIZE+Game.GB_X, position.getY()*Game.GRID_SIZE+Game.GB_Y, 
+                Game.GRID_SIZE*LENGTH, Game.GRID_SIZE*HEIGHT, null);
+    }
+
+    @Override
+    public Rectangle getBound() {
+        return new Rectangle(position.getX(), position.getY(), LENGTH, HEIGHT);
+    }
+
+    @Override
+    public List<Position> getPositions() {
+        List<Position> returnList = new ArrayList<Position>();
+        returnList.add(position);
+        return returnList;
+    }
+    
+}
